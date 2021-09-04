6efbc16d6763917fd21be4adf0062a0971e6fafb
Eduardo Castro <eduardocastro91@gmail.com>
2021-09-03 22:44:55 -0300

    .

--- 2021.1_AED/Lista01/exercicio01.py
+++
@@ -1,8 +0,0 @@
-# Exercício 01
-# Escreva um programa que leia alguma coisa do teclado e escreva esta coisa.
-
-# Input: Alô Mundo! Alô UnB. AlOOOOooo.......
-# Output: Alô Mundo! Alô UnB. AlOOOOooo.......
-
-message = input()
-print(message)
--- 2021.1_AED/Lista01/exercicio06.py
+++
@@ -1,5 +0,0 @@
-number = int(input())
-
-for iterator in range(1,number+1):
-    if number % iterator == 0:
-        print(iterator)
